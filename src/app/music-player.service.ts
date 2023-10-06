e03759062dbfef91d2bffc506430716c01676412
srivasthavi.m <srivasthavi.m@prodapt.com>
2023-10-06 05:45:34 +0530

    added add to favourites

--- src/app/music-player.service.ts
+++ src/app/music-player.service.ts
@@ -10,11 +10,15 @@ export class MusicPlayerService {
   private playlistDescriptionSubject = new BehaviorSubject<string>('');
   private playlistImageSubject = new BehaviorSubject<string>('');
 
+
   playlistName$ = this.playlistNameSubject.asObservable();
   playlistDescription$ = this.playlistDescriptionSubject.asObservable();
   playlistImage$ = this.playlistImageSubject.asObservable();
 
-  setPlaylistData(name: string, description: string, imgsource: string) {
+
+
+  setPlaylistData( name: string, description: string, imgsource: string) {
+  
     this.playlistNameSubject.next(name);
     this.playlistDescriptionSubject.next(description);
     this.playlistImageSubject.next(imgsource);
